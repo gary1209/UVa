9c1a554f3433dbf15909976accda8594db22a9dc
GaryHsu <b01608011@ntu.edu.tw>
2018-07-28 02:38:29 +0800

    Create 100.cpp

---
+++ 100.cpp
@@ -0,0 +1,23 @@
+#include <cstdio>
+
+int main(){
+  while(1)
+  {
+    int num1, num2; 
+    scanf("%d%d", &num1,&num2);
+    // printf("%d,%d\n",num1,num2);
+    int max_cycle_length = 1;
+    for(int i= num1; i<=num2; i++)
+    {
+      int j = i;
+      int cycle_length = 1;
+      while(j != 1){
+        j%2==0 ? j/=2 : j=j*3+1;
+        cycle_length++;
+      }
+      // printf("cycle_length:%d!\n",cycle_length);
+      max_cycle_length = max_cycle_length>cycle_length?max_cycle_length:cycle_length;
+    }
+    printf("%d %d %d\n",num1,num2,max_cycle_length);
+  }
+}
