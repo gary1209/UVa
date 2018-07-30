4a8314ee2caac56e6d8428a1de6d373bee4e0f05
GaryHsu <b01608011@ntu.edu.tw>
2018-07-30 20:10:22 +0800

    Update 913.cpp

--- 913.cpp
+++ 913.cpp
@@ -1,12 +1,22 @@
 #include <cstdio>
 #include <cmath>
 
+#int main(){
+#  while(1){
+#    int num1;
+#    scanf("%d", &num1);
+#    int last_num;
+    #last_num = ((int)pow(num1+1,2) / 4)*2 - 1; long long 情況下 pow 會失準
+#    printf("%d\n", last_num*3 -6);
+#  }
+#}
+
 int main(){
   while(1){
-    int num1;
-    scanf("%d", &num1);
-    int last_num;
-    last_num = ((int)pow(num1+1,2) / 4)*2 - 1;
-    printf("%d\n", last_num*3 -6);
+    long long num1;
+    if(scanf("%lld", &num1)!=1) break;
+    long long last_num;
+    last_num = (num1+1)*(num1+1) / 2 - 1;
+    printf("%lld\n", last_num*3 -6);
   }
 }
