7432cfdde06ae7d8b9059f71be05018c1b0d3d92
GaryHsu <b01608011@ntu.edu.tw>
2018-08-12 17:21:00 +0800

    Create problem.cpp

---
+++ problem.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+
+int main(){
+    char s2[1000];
+    int c;
+    while(true){
+        printf("strat\n");
+        if(scanf("%[^\n]",s2) != 1) break;
+        printf("%s",s2)    
+    }
+}
