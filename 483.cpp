07ca9b3428f0e83940c927e5773b4c3cf3be9301
GaryHsu <b01608011@ntu.edu.tw>
2018-08-20 19:02:29 +0800

    Update 483.cpp

--- 483.cpp
+++ 483.cpp
@@ -9,7 +9,7 @@ int main(){
   
   while(1){
     if(scanf("%c",&c) != 1) break;
-    if(c == '\n' || c == ' '){);
+    if(c == '\n' || c == ' '){
       for(int i = index-1; i >= 0; i--) printf("%c",s[i]);
       printf("%c",c);
       index = 0;
