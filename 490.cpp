32b254aa2fab99fd85a11654d42ed6c46516a618
GaryHsu <b01608011@ntu.edu.tw>
2018-09-07 13:18:39 +0800

    Update 490.cpp

--- 490.cpp
+++ 490.cpp
@@ -1,37 +1,40 @@
 #include <cstdio>
-#include <cstring>
 
 int main(){
-  char a[100][100] = {'\n'};
+
+  char a[101][101] = {'\0'};
+  int i = 0;
+  int j = 0;
+  int max_row = 0;
+  int max_col = 0;
   char c;
-  int i=0;
-  int j=0;
   
-  //先input
   while(1){
-      // printf("%c",c);
-      if(scanf("%c",&c) != 1) break;
-      if(c != '\n'){
-        a[i][j] = c;
-        i++;
-      }else{ //換行
-        j++;
-        i = 0;
-      }
-  }
-  
-  //再output  
-  for(j=0;j<99;j++){
-    for(i=0;i<99;i++){
-      if(a[i][j]!='\n'){
-        printf("%c",a[i][j]);
-      }else{
-        printf("\n");
-      }
+    if((scanf("%c",&c)) != 1) break;
+    if(max_row ==0) max_row=1;
+    if(max_col ==0) max_col=1;
+    a[i][j] = c;
+    
+    if(c != '\n'){
+      j++;
+      if(j>max_col) max_col = j;
+    }else{
+      j=0;
+      i++;
+      if(i>max_row) max_row = i;
     }
   }
+  max_row++;
+  // printf("max_col:%d\n",max_col);
+  // printf("max_row:%d\n",max_row);
+  for(int m = 0; m < max_col; m++){
+    for(int k = max_row-1; k >= 0; k--){
+      if(a[k][m] != '\n') printf("%c",a[k][m]);
+      else printf(" ");
+    }
+    if(m != max_col-1) printf("\n");
+  }
 }
-
 
 /*
 #include <cstdio>
