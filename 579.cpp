f6f1ba43b124ea9cb81624c04e37fc9c5679375b
GaryHsu <b01608011@ntu.edu.tw>
2018-09-08 21:25:00 +0800

    Update 579.cpp

--- 579.cpp
+++ 579.cpp
@@ -1,32 +1,44 @@
 #include <cstdio>
+#include <cstring>
+// http://www.cplusplus.com/reference/cstring/strcmp/
+
+// int Strcmp(const char *a, const char *b) {
+//   while(*a == *b) {
+//     if(*a == '\0') return 0;
+//     a++;
+//     b++;
+//   }
+//   return *a - *b;
+// }
 
 int main(){
-  char str1[10];
-  char stop[10] = "0:00";
-  char hh[2];
-  char mm[2];
+  // char str1[10];
+  //char stop[10] = "0:00";
+  // char hh[3];
+  // char mm[3];
   
+
+  
+  // scanf("%d%*c%d"
   while(1){
-    if(scanf("%s",str1)!=1) break;
-    if(str1 == stop) break;
-    
-    sscanf(str1,"%s:%s",hh,mm);
-    printf("%s\n%s\n",hh,mm);
-    
     int h;
     int m;
-    float angle;
     
-    h = atoi(hh);
-    m = atoi(mm);
+    if(scanf("%d%*c%d",&h,&m)!=2) break;
+    // if(strcmp(str1,"0:00")==0) break;
+    if(h==0 && m==0) break; 
     
-    angle = (h*60 + m)*0.5 - m*6
-    while(angle > 180){
-      angle-=180;
+    double angle;
+    
+    // h = atoi(hh);
+    // m = atoi(mm);
+    // printf("%d,%d\n",h,m);
+    angle = (h*60 + m)*0.5 - m*6;
+    while(angle > 180 || angle < 0){
+      if (angle>180) angle= 360 - angle;
+      if (angle<0) angle = 360 + angle;
     }
-    while(angle < 0){
-      angle+=180;  
-    }
-    printf("%3lf\n",angle);
+  
+    printf("%.3f\n",angle);
   }
 }
